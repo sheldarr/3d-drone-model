887bc176fa0539730ce809aa715a5d59e1b97db8
Rafal Ostrowski <ostrowskirafal1990@gmail.com>
2016-06-26 22:24:18 +0200

    Added wings rotation.

---
+++ UNITY/drone/Assets/GameInit.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameInit : MonoBehaviour
+{
+	public GameObject DroneModel;
+	public GameObject Wing1;
+	public GameObject Wing2;
+	public GameObject Wing3;
+	public GameObject Wing4;
+
+	// Use this for initialization
+	void Start () {
+		DroneModel.GetComponent<Renderer>().material.color = Color.black;
+		Wing1.GetComponent<Renderer>().material.color = Color.red;
+		Wing2.GetComponent<Renderer>().material.color = Color.red;
+		Wing3.GetComponent<Renderer>().material.color = Color.red;
+		Wing4.GetComponent<Renderer>().material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
